00f1e7f357e97548adec37c64c9a25414db0363e
Matti Besser <mail@mattibesser.com>
2015-12-09 22:47:40 +0100

    feat: Add big chunk of not properly committed changes.

--- _includes/javascript/analytics.js
+++ _includes/javascript/analytics.js
@@ -1,5 +1,3 @@
-<script type="text/javascript">
-
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-35701346-1']);
 _gaq.push(['_setDomainName', 'mattibesser.com']);
@@ -10,5 +8,3 @@ _gaq.push(['_trackPageview']);
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
-
-</script>
